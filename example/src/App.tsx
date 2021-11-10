d1cbbc7dc4c9bdbb29876ee12990bea529d6c78a
Ashwin kumar <ashwin.gps@gmail.com>
2021-11-10 17:52:00 +0530

    fix : non-visible stripes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import 'react-progress-bar/dist/index.css'
 const App = () => {
   return (
     <div style={{ width: '750px', margin: '40px' }}>
-      <ProgressBar value={30} color='red' />
+      <ProgressBar value={30} isStriped={true} />
     </div>
   )
 }
